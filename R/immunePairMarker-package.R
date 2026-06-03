90ebbc11819ae2bf180500565e64026458f6a244
twl-00 <tanwenlu2000@163.com>
2026-06-03 15:23:08 +0800

    Add generated documentation

---
+++ R/immunePairMarker-package.R
@@ -0,0 +1,8 @@
+#' immunePairMarker
+#'
+#' Tools for pairwise gene marker analysis in immune response studies.
+#'
+#' @keywords internal
+#' @useDynLib immunePairMarker, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+"_PACKAGE"
